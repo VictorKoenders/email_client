cc518d5c175a90b4ce6c6076c46c1c6771e438f9
Victor Koenders <victor.koenders@gmail.com>
2018-08-22 16:15:37 +0200

    Fixed an issue where the web server would only listen internally

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -26,7 +26,7 @@ pub fn serve(addr: Addr<WebsocketServer>, database: Addr<Database>) {
             .resource("/bundle.js", |r| r.f(bundle))
             .resource("/bundle.js.map", |r| r.f(bundle_map))
             .resource("/ws/", |r| r.f(ws_start))
-        }).bind("127.0.0.1:8001")
+        }).bind("0.0.0.0:8001")
         .expect("Can not bind on port 8001")
         .run();
     });
