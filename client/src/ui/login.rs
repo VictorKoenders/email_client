f698708fbd810ae46b7fb52be4a4d1b51f08a959
Victor Koenders <victor.koenders@gmail.com>
2018-10-22 20:28:17 +0200

    Working login page

--- client/src/ui/login.rs
+++ client/src/ui/login.rs
@@ -1,10 +1,18 @@
 use shared::login::LoginRequest;
+use std::mem;
 use yew::prelude::*;
 
-pub enum Msg {}
+pub enum Msg {
+    Nope,
+    UsernameChanged(String),
+    PasswordChanged(String),
+    Submit,
+}
 
 pub struct Login {
     pub on_attempt_login: Option<Callback<LoginRequest>>,
+    pub username: String,
+    pub password: String,
 }
 
 #[derive(Default, Clone, PartialEq)]
@@ -19,6 +27,8 @@ impl Component for Login {
     fn create(props: Self::Properties, _: ComponentLink<Self>) -> Self {
         Self {
             on_attempt_login: props.on_attempt_login,
+            username: String::new(),
+            password: String::new(),
         }
     }
 
@@ -28,14 +38,65 @@ impl Component for Login {
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        match msg {}
+        match msg {
+            Msg::Nope => false,
+            Msg::UsernameChanged(un) => {
+                self.username = un;
+                false
+            }
+            Msg::PasswordChanged(pw) => {
+                self.password = pw;
+                false
+            }
+            Msg::Submit => {
+                let request = LoginRequest {
+                    username: mem::replace(&mut self.username, String::new()),
+                    password: mem::replace(&mut self.password, String::new()),
+                };
+
+                if let Some(on_attempt_login) = &self.on_attempt_login {
+                    on_attempt_login.emit(request);
+                }
+                true
+            }
+        }
     }
 }
 
 impl Renderable<Login> for Login {
     fn view(&self) -> Html<Login> {
         html! {
-            <b>{ "Hello from Login" }</b>
+            <div class="card", style="width: 30rem; margin: auto;", >
+                <div class="card-header", >{"Log in"}</div>
+                <div class="card-body", >
+                    <div class="form-group",>
+                        <label for="username", >{ "Username" }</label>
+                        <input
+                            type="text",
+                            class="form-control",
+                            value=&self.username,
+                            autoFocus=true,
+                            oninput=|e| Msg::UsernameChanged(e.value),
+                            onkeypress=|e| {
+                                if e.key() == "Enter" { Msg::Submit } else { Msg::Nope }
+                            },
+                        />
+                    </div>
+                    <div class="form-group",>
+                        <label for="password", >{ "Password" }</label>
+                        <input
+                            type="password",
+                            class="form-control",
+                            value=&self.password,
+                            oninput=|e| Msg::PasswordChanged(e.value),
+                            onkeypress=|e| {
+                                if e.key() == "Enter" { Msg::Submit } else { Msg::Nope }
+                            },
+                        />
+                    </div>
+                    <button class=("btn", "btn-primary"), type="button", onclick=|_| Msg::Submit, >{"Log in"}</button>
+                </div>
+            </div>
         }
     }
 }
